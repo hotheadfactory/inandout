9fce0a6d16b27e38a0247e727b75626220d7a4f9
Jung Hoe Hyeong <commaniakr@gmail.com>
2019-07-10 00:28:22 +0900

    Fix

--- kiosk/app.js
+++ kiosk/app.js
@@ -10,16 +10,17 @@ var connection = mysql.createConnection({
 });
 var rfid=require('node-rfid');
 
-rfid.read(function(err,result){
-  if(err) console.log("Sorry, some hardware error occurred"); //some kind of hardware/wire error
-  console.log(result); //print rfid tag UID
-});
 const port = 80;
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log("Server is on (Port "+port+")");
+
+    rfid.read(function(err,result){
+      if(err) console.log("Sorry, some hardware error occurred"); //some kind of hardware/wire error
+      console.log(result); //print rfid tag UID
+    });
 });
 
 connection.connect();
