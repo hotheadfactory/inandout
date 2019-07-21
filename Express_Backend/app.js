9c3dcf5e9b0258bcf968a5da62c702526623bf8d
Asua Hostin <a01054394111@gmail.com>
2019-07-21 19:25:37 +0900

    feat : login

--- Express_Backend/app.js
+++ Express_Backend/app.js
@@ -2,13 +2,14 @@ var express = require("express");
 var path = (path = require("path"));
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
+var jwt = require("jsonwebtoken");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "1lost5egg!",
-  database: "moment",
+  host: "168.131.35.102",
+  user: "innout",
+  password: "innout0512!",
+  database: "innoutdb",
   debug: false
 });
 var app = express();
@@ -18,135 +19,66 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "content-type, x-access-token"); //1
+  res.header("Access-Control-Allow-Headers", "content-type, x-access-token");
   next();
 });
 
-app.post("/process/adduser", function(req, res) {
-  console.log("/process/adduser 호출됨.");
-
-  var paramEmail = req.param("inputEmail");
-  var paramName = req.param("name");
-  var paramPassword = req.param("inputPassword");
-  var paramNick = req.param("Nick");
-
-  if (pool) {
-    addUser(paramEmail, paramNick, paramName, paramPassword, function(
-      err,
-      result
-    ) {
-      if (err) {
-      }
-
-      if (result) {
-        console.dir(result);
-
-        console.log("inserted " + result.affectedRows + " rows");
-
-        var insertId = result.name;
-        console.log("추가한 레코드의 아이디 : " + insertId);
-
-        res.redirect("/Login/join.html");
-        res.end();
-      } else {
-        res.redirect("/");
-        res.end();
-      }
-    });
-  } else {
-    res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-    res.redirect("/");
-    res.end();
-  }
-});
 //로그인
-
-app.post("/process/login", function(req, res) {
-  console.log("/process/login 호출됨.");
-
-  var paramEmail = req.param("inputEmail");
-  var paramPassword = req.param("inputPassword");
-  console.log("이미로그인 되어있음");
-  if (pool) {
-    if (req.session.user) {
-      console.log("이미로그인 되어있음");
-    } else {
-      authUser(paramEmail, paramPassword, function(err, rows) {
-        if (err) {
-          throw err;
-        }
-
-        if (rows) {
-          console.dir(rows[0].Nick);
-
-          req.session.user = {
-            id: paramEmail,
-            pw: paramPassword,
-            name: rows[0].Nick,
-            authorized: true
-          };
-          res.redirect("/");
-          res.end();
-        } else {
-          res.redirect("/Login/login.html");
-          res.end();
-        }
-      });
-    }
-  } else {
-    res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-    res.end();
-  }
-});
-
-//사용자를 등록하는 함수
-var addUser = function(email, Nick, name, password, callback) {
-  console.log("addUser 호출됨.");
-
-  // 커넥션 풀에서 연결 객체를 가져옵니다.
-  pool.getConnection(function(err, conn) {
-    if (err) {
-      conn.release(); // 반드시 해제해야 합니다.
-      return;
-    }
-    console.log("데이터베이스 연결 스레드 아이디 : " + conn.threadId);
-
-    // 데이터를 객체로 만듭니다.
-    var data = { email: email, name: name, password: password, Nick: Nick };
-
-    // SQL 문을 실행합니다.
-    var exec = conn.query("insert into users set ?", data, function(
-      err,
-      result
-    ) {
-      conn.release(); // 반드시 해제해야 합니다.
-      console.log("실행 대상 SQL : " + exec.sql);
-
-      if (err) {
-        console.log("SQL 실행 시 에러 발생함.");
-        console.dir(err);
-
-        callback(err, null);
-
-        return;
-      }
-
-      callback(null, result);
-    });
-
-    conn.on("error", function(err) {
-      console.log("데이터베이스 연결 시 에러 발생함.");
-      console.dir(err);
-
-      callback(err, null);
-    });
-  });
+var successTrue = function(data) {
+  //
+  return {
+    success: true,
+    message: null,
+    errors: null,
+    data: data
+  };
 };
 
-// 로그인 처리 함수
-var authUser = function(email, password, callback) {
-  console.log("authUser 호출됨 : " + email + "," + password);
+var successFalse = function(err, message) {
+  if (!err && !message) message = "data not found";
+  return {
+    success: false,
+    message: message,
+    errors: err ? util.parseError(err) : null,
+    data: null
+  };
+};
+app.post("/process/login", function(req, res) {
+  console.log("/process/login 호출됨.");
+  console.log(req.body);
+  var paramId = req.body["id"];
+  var paramPassword = req.body["password"];
+  var paramType = req.body["type"];
 
+  if (pool) {
+    authUser(paramId, paramPassword, paramType, function(err, rows) {
+      if (err) {
+        throw err;
+      }
+
+      if (rows) {
+        var id = rows[0][`${paramType}id`];
+        console.dir(rows);
+        var payload = {
+          id: id,
+          username: rows[0].username
+        };
+        var secretOrPrivateKey = "innout";
+        jwt.sign(payload, secretOrPrivateKey, function(err, token) {
+          if (err) return res.json(successFalse(err));
+          res.json(successTrue(token));
+          res.end();
+        });
+      }
+    });
+  } else {
+    res.writeHead("500");
+    res.end();
+  }
+});
+
+// 로그인 처리 함수
+var authUser = function(id, password, type, callback) {
   pool.getConnection(function(err, conn) {
     if (err) {
       if (conn) {
@@ -156,15 +88,14 @@ var authUser = function(email, password, callback) {
       return;
     }
     console.log("데이어베이스 연결 스레드 아이디 : " + conn.threadId);
-    var tablename = "users";
-    var culumns = ["email", "name", "Nick"];
+    var tablename = type;
+    var culumns = [`${type}id`, "username"];
     var exec = conn.query(
-      "select ?? from ?? where email = ? and password = ?",
-      [culumns, tablename, email, password],
+      `select ?? from ?? where ${type}id = ? and pin = ?`,
+      [culumns, tablename, id, password],
       function(err, rows) {
         conn.release();
         console.log("실행된 SQL : " + exec.sql);
-
         if (err) {
           callback(err, null);
           return;
