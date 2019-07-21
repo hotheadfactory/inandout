0f03423afcd753f0578429ba232a3bfbc7a0207a
Asua Hostin <a01054394111@gmail.com>
2019-07-21 21:10:11 +0900

    feat : join, reservation

--- Express_Backend/app.js
+++ Express_Backend/app.js
@@ -76,7 +76,6 @@ app.post("/process/login", function(req, res) {
     res.end();
   }
 });
-
 // 로그인 처리 함수
 var authUser = function(id, password, type, callback) {
   pool.getConnection(function(err, conn) {
@@ -115,6 +114,149 @@ var authUser = function(id, password, type, callback) {
 
       callback(err, null);
     });
+  });
+};
+//member 가입
+app.post("/process/join/member", function(req, res) {
+  let paramId = req.body.memberid;
+  let parampin = req.body.pin;
+  let paramName = req.body.username;
+  if (pool) {
+    joinUser(paramId, parampin, paramName, function(err, result) {
+      if (err) {
+        console.log("errer", err);
+        res.send({
+          code: 400,
+          success: false
+        });
+      } else {
+        console.log("errer", result);
+        res.send({
+          code: 200,
+          success: true
+        });
+      }
+    });
+  }
+});
+
+const joinUser = function(id, pin, name, callback) {
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      if (conn) {
+        conn.release();
+      }
+      callback(err, null);
+      return;
+    }
+    console.log("데이어베이스 연결 스레드 아이디 : " + conn.threadId);
+    const tablename = "member";
+    const culumns = ["memberid", "pin", "username"];
+    var exec = conn.query(
+      `insert into ${tablename}(??) values("${id}","${pin}","${name}")`,
+      [culumns],
+      function(err, rows) {
+        console.log(rows);
+        conn.release();
+        console.log("실행된 SQL : " + exec.sql);
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        if (rows.affectedRows > 0) {
+          console.log("성공");
+          callback(null, rows);
+        } else {
+          console.log("사용자 찾지못함");
+          callback(null, null);
+        }
+      }
+    );
+    conn.on("error", function(err) {
+      console.log("데이터베이스 연결 시 에러 발생함.");
+      console.dir(err);
+
+      callback(err, null);
+    });
+  });
+};
+
+app.post("/process/reservation", function(req, res) {
+  let paramDate = req.body.date;
+  let paramMemberId = req.body.memberid;
+  let paramUsername = req.body.username;
+  let paramORID = req.body.organizationId;
+  let paramORNAME = req.body.organizationName;
+  if (pool) {
+    reservation(
+      paramDate,
+      paramMemberId,
+      paramUsername,
+      paramORID,
+      paramORNAME,
+      function(err, result) {
+        if (err) {
+          console.log("errer", err);
+          res.send({
+            code: 400,
+            success: false
+          });
+        } else {
+          console.log("errer", result);
+          res.send({
+            code: 200,
+            success: true
+          });
+        }
+      }
+    );
+  }
+});
+
+const reservation = function(
+  date,
+  memberid,
+  username,
+  organizationId,
+  organizationName,
+  callback
+) {
+  pool.getConnection(function(err, conn) {
+    if (err) {
+      if (conn) {
+        conn.release();
+      }
+      callback(err, null);
+      return;
+    }
+    console.log("데이어베이스 연결 스레드 아이디 : " + conn.threadId);
+    const tablename = "reservation";
+    const culumns = [
+      "memberid",
+      "username",
+      "organizationId",
+      "organizationName",
+      "intime"
+    ];
+    let exec = conn.query(
+      `insert into ${tablename}(??) values("${memberid}","${username}",${organizationId},"${organizationName}",DATE "${date}")`,
+      [culumns],
+      function(err, rows) {
+        conn.release();
+        console.log("실행된 SQL : " + exec.sql);
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        if (rows.affectedRows > 0) {
+          console.log("성공");
+          callback(null, rows);
+        } else {
+          console.log("찾지못함");
+          callback(null, null);
+        }
+      }
+    );
   });
 };
 
