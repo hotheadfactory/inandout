f45dd06a073d09c914a447498bf9d2c3ddafd9d0
Asua Hostin <a01054394111@gmail.com>
2019-09-18 00:43:57 +0900

    feat : token 해석하는 리퀘스트 핸들러 구현
      키오스크나 웹에서 로그인시 필요한 정보가 암호화 되어있으므로 필요한 정보만 해독해서 받아오는 기능이 필요해서 구현함

--- Express_Backend/app.js
+++ Express_Backend/app.js
@@ -24,6 +24,15 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get("/token", function(req, res) {
+  const token = req.query.token;
+  if (!token) res.status(401).send("Unauthorized: No token provided");
+  jwt.verify(token, SECRET_KEY.jwt, function(err, decoded) {
+    if (err) res.status(401).send("Unauthorized: Invalid token");
+    res.status(200).send({ memberid: decoded.id, username: decoded.username });
+  });
+});
+
 //로그인
 const successTrue = function(data) {
   return {
@@ -77,7 +86,7 @@ const authUser = function(id, password, type) {
       }
       console.log("데이어베이스 연결 스레드 아이디 : ", connection.threadId);
       const tableName = type;
-      const culumns = [`${type}id`, "username"];
+      const culumns = [`${type}id`, "username", "asigned"];
       const executeSql = connection.query(
         `select ?? from ?? where ${type}id = ? and pin = ?`,
         [culumns, tableName, id, password],
@@ -126,6 +135,75 @@ const joinUser = function(id, pin, name) {
           connection.release();
           if (err) return reject(err);
           if (rows.affectedRows > 0) return resolve(rows.affectedRows);
+        }
+      );
+    });
+  });
+};
+
+app.post("/process/card/login", async function(req, res) {
+  let paramCardNumber = req.body.cardnumber;
+  if (!pool)
+    res.status(500).send({ status: 500, message: "데이터베이스에 연결되어 있지 않습니다." });
+  try {
+    const result1 = await findMemberid(paramCardNumber);
+    if (!result1) throw new Error("등록되지 않은 카드입니다.");
+    const memberid = result1[0]["memberid"];
+    const result2 = await findUsername(memberid);
+    if (!result2) throw new Error("일치하는 회원정보가 존재하지 않습니다.");
+    if (!result2[0]["asigned"]) throw new Error("아직 승인되지 않은 회원입니다.");
+    const username = result2[0]["username"];
+    const payload = {
+      id: memberid,
+      username: username
+    };
+    jwt.sign(payload, SECRET_KEY.jwt, { expiresIn: 86400 }, (err, token) => {
+      if (err) res.status(200).json(successFalse(err));
+      res.status(200).json(successTrue(token));
+    });
+  } catch (e) {
+    res.status(400).send({ status: 400, message: e.message });
+  }
+});
+
+const findMemberid = function(cardnumber) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        if (connection) connection.release();
+        return reject(err);
+      }
+      console.log("데이어베이스 연결 스레드 아이디 : ", connection.threadId);
+      const tableName = "card";
+      const executeSql = connection.query(
+        `select memberid from ${tableName} where cardnumber="${cardnumber}"`,
+        (err, rows) => {
+          console.log("실행된 sql : ", executeSql.sql);
+          if (err) return reject(err);
+          if (rows.length > 0) return resolve(rows);
+          resolve(null);
+        }
+      );
+    });
+  });
+};
+
+const findUsername = function(memberid) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        if (connection) connection.release();
+        return reject(err);
+      }
+      console.log("데이어베이스 연결 스레드 아이디 : ", connection.threadId);
+      const tableName = "member";
+      const executeSql = connection.query(
+        `select username, asigned from ${tableName} where memberid="${memberid}"`,
+        (err, rows) => {
+          console.log("실행된 sql : ", executeSql.sql);
+          if (err) return reject(err);
+          if (rows.length > 0) return resolve(rows);
+          resolve(null);
         }
       );
     });
